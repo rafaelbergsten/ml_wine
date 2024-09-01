b1c320131b52a87ade53701309dd21d854c9d428
Rafael Elias <21204797+rafaelbergsten@users.noreply.github.com>
2024-09-01 01:46:23 -0300

    Applied best coding practices...

--- app.py
+++ app.py
@@ -1,35 +1,40 @@
-from flask import Flask, jsonify, request
-from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
-import requests
-from flasgger import Swagger
-from dotenv import load_dotenv
+import logging
 import os
+from logging.handlers import RotatingFileHandler
+
+import requests
 from bs4 import BeautifulSoup
+from flasgger import Swagger
+from flask import Flask, jsonify
+from flask_jwt_extended import JWTManager, jwt_required
 
 app = Flask(__name__)
 swagger = Swagger(app)
 
-app.config['JWT_SECRET_KEY'] = os.getenv('JWT_SECRET_KEY')
+app.config["JWT_SECRET_KEY"] = os.getenv("JWT_SECRET_KEY")
 jwt = JWTManager(app)
 
+
 def extract_table_data(html_content):
-    soup = BeautifulSoup(html_content, 'html.parser')
-
-    tables = soup.find_all('table', {'class': 'tb_base tb_dados'})
-
+    soup = BeautifulSoup(html_content, "html.parser")
+    tables = soup.find_all("table", {"class": "tb_base tb_dados"})
     extracted_data = []
 
     for table in tables:
-        headers = [th.get_text(strip=True) for th in table.find('thead').find_all('th')]
-        rows = table.find('tbody').find_all('tr')
+        headers = [th.get_text(strip=True) for th in table.find("thead").find_all("th")]
+        rows = table.find("tbody").find_all("tr")
 
         for row in rows:
-            cells = row.find_all('td')
+            cells = row.find_all("td")
             if len(cells) == len(headers):
-                row_data = {headers[i]: cells[i].get_text(strip=True) for i in range(len(headers))}
+                row_data = {
+                    headers[i]: cells[i].get_text(strip=True)
+                    for i in range(len(headers))
+                }
                 extracted_data.append(row_data)
 
     return extracted_data
+
 
 def fetch_and_process_data(url):
     response = requests.get(url)
@@ -40,8 +45,27 @@ def fetch_and_process_data(url):
     else:
         return {"error": "Failed to retrieve data"}
 
-# Endpoint para dados de Produção
-@app.route('/api/producao', methods=['GET'])
+
+if not app.debug:
+    if not os.path.exists("logs"):
+        os.mkdir("logs")
+
+    file_handler = RotatingFileHandler(
+        "logs/flask_app.log", maxBytes=10240, backupCount=10
+    )
+    file_handler.setFormatter(
+        logging.Formatter(
+            "%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]"
+        )
+    )
+    file_handler.setLevel(logging.INFO)
+    app.logger.addHandler(file_handler)
+
+    app.logger.setLevel(logging.INFO)
+    app.logger.info("Application startup")
+
+
+@app.route("/api/producao", methods=["GET"])
 @jwt_required()
 def producao():
     """
@@ -57,12 +81,12 @@ def producao():
               type: string
               description: The data returned from Embrapa
     """
-    url = os.getenv('EMBRAPA_URL_PRODUCAO')
+    url = os.getenv("EMBRAPA_URL_PRODUCAO")
     data = fetch_and_process_data(url)
     return jsonify({"data": data})
 
-# Endpoint para dados de Processamento
-@app.route('/api/processamento', methods=['GET'])
+
+@app.route("/api/processamento", methods=["GET"])
 @jwt_required()
 def processamento():
     """
@@ -78,12 +102,12 @@ def processamento():
               type: string
               description: The data returned from Embrapa
     """
-    url = os.getenv('EMBRAPA_URL_PROCESSAMENTO')
+    url = os.getenv("EMBRAPA_URL_PROCESSAMENTO")
     data = fetch_and_process_data(url)
     return jsonify({"data": data})
 
-# Endpoint para dados de Comercialização
-@app.route('/api/comercializacao', methods=['GET'])
+
+@app.route("/api/comercializacao", methods=["GET"])
 @jwt_required()
 def comercializacao():
     """
@@ -99,12 +123,12 @@ def comercializacao():
               type: string
               description: The data returned from Embrapa
     """
-    url = os.getenv('EMBRAPA_URL_COMERCIALIZACAO')
+    url = os.getenv("EMBRAPA_URL_COMERCIALIZACAO")
     data = fetch_and_process_data(url)
     return jsonify({"data": data})
 
-# Endpoint para dados de Importação
-@app.route('/api/importacao', methods=['GET'])
+
+@app.route("/api/importacao", methods=["GET"])
 @jwt_required()
 def importacao():
     """
@@ -120,12 +144,12 @@ def importacao():
               type: string
               description: The data returned from Embrapa
     """
-    url = os.getenv('EMBRAPA_URL_IMPORTACAO')
+    url = os.getenv("EMBRAPA_URL_IMPORTACAO")
     data = fetch_and_process_data(url)
     return jsonify({"data": data})
 
-# Endpoint para dados de Exportação
-@app.route('/api/exportacao', methods=['GET'])
+
+@app.route("/api/exportacao", methods=["GET"])
 @jwt_required()
 def exportacao():
     """
@@ -141,9 +165,10 @@ def exportacao():
               type: string
               description: The data returned from Embrapa
     """
-    url = os.getenv('EMBRAPA_URL_EXPORTACAO')
+    url = os.getenv("EMBRAPA_URL_EXPORTACAO")
     data = fetch_and_process_data(url)
     return jsonify({"data": data})
 
-if __name__ == '__main__':
-    app.run(debug=True)
+
+if __name__ == "__main__":
+    app.run()
